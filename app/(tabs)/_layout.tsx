1c1ef39b105851e20522bc4f229fc5e588253e8b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-20 10:41:29 +0530

    updated layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,58 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import Colors from '@/constants/Colors'; 
+import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const Layout = () => {
   return (
-    <Tabs>
-      
+    <Tabs
+        screenOptions={{
+            tabBarActiveTintColor: Colors.primary,
+            tabBarLabelStyle: {
+                fontFamily: 'mon-sb',
+            }
+        }}
+    >
+        <Tabs.Screen
+            name="index"
+            options={{
+                tabBarLabel: 'Explore',
+                tabBarIcon: ({color, size}) => <Ionicons name="search" color={color} size={size}/>
+            }}
+        />
+        <Tabs.Screen
+            name="wishlists"
+            options={{
+                tabBarLabel: 'Wishlists',
+                tabBarIcon: ({color, size}) => <Ionicons name="heart-outline" color={color} size={size}/>
+            }}
+        />
+        <Tabs.Screen
+            name="trips"
+            options={{
+                tabBarLabel: 'Trips',
+                tabBarIcon: ({color, size}) => <FontAwesome5 name="airbnb" color={color} size={size}/>
+            }}
+        />
+        <Tabs.Screen
+            name="inbox"
+            options={{
+                tabBarLabel: 'Inbox',
+                tabBarIcon: ({color, size}) => <MaterialCommunityIcons name="message-outline" color={color} size={size}/>
+            }}
+        />
+        <Tabs.Screen
+            name="profile"
+            options={{
+                tabBarLabel: 'Profile',
+                tabBarIcon: ({color, size}) => <Ionicons name="person-circle" color={color} size={size}/>
+            }}
+        />
     </Tabs>
   )
-}
+};
 
-export default Layout
+export default Layout;
