a0c5d5a315a9808267da893a5c626d52a8a28831
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-21 12:54:10 +0530

    updated profile

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,15 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Button } from 'react-native'
+import React from 'react';
+import { useAuth } from "@clerk/clerk-react";
 
 const Page = () => {
+  const { signOut, isSignedIn } = useAuth();
   return (
     <View>
-      <Text>Profile</Text>
+      <Button>
+        {isSignedIn && 
+        <}
+      </Button>
     </View>
   )
 }
