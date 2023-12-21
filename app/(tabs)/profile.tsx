be82db9920c0218130d54288232331a1ade9b3a0
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-21 13:21:04 +0530

    updated profile

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,15 +1,18 @@
 import { View, Text, Button } from 'react-native'
 import React from 'react';
 import { useAuth } from "@clerk/clerk-react";
+import {Link} from 'expo-router';
 
 const Page = () => {
   const { signOut, isSignedIn } = useAuth();
   return (
     <View>
-      <Button>
-        {isSignedIn && 
-        <}
-      </Button>
+      <Button title="Log out" onPress={() => signOut()}/>
+        {!isSignedIn && (
+          <Link href={'/(modals)/login'}>
+            <Text>Log In</Text>
+          </Link>
+        )}
     </View>
   )
 }
