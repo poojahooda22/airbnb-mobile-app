8720f3d00efaa563cbc501e7b7e600d3e299309f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-22 01:16:47 +0530

    updated index

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,15 +2,17 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { Link, Stack } from 'expo-router';
 import ExploreHeader from '@/components/ExploreHeader';
+import Listings from '@/components/Listings';
 
 const Page = () => {
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <Stack.Screen 
         options={{
           header: () => <ExploreHeader/>
         }}
       />
+      <Listings />
     </View>
   )
 }
