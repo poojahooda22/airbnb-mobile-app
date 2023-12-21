8082dd9b0eb05b84b787378674279c610446b0d2
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-21 23:20:29 +0530

    created exploreHeader

---
+++ components/ExploreHeader.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ExploreHeader = () => {
+  return (
+    <View>
+      <Text>ExploreHeader</Text>
+    </View>
+  )
+}
+
+export default ExploreHeader;
