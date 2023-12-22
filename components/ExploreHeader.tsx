90d65726461e3afa5b4d5fc35f05db8b7b44ae77
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-22 22:09:57 +0530

    updated header

--- components/ExploreHeader.tsx
+++ components/ExploreHeader.tsx
@@ -41,7 +41,9 @@ const categories = [
 const ExploreHeader = () => { 
   return (
     <SafeAreaView 
-        style={{ flex: 1, backgroundColor: '#fff', marginTop: 32}}>
+        style={{ 
+            flex: 1, 
+            backgroundColor: '#fff', marginTop: 32}}>
         <View style={styles.container}>
             <View style={styles.actionRow}>
                 <Link href={'/(modals)/booking'} asChild>
