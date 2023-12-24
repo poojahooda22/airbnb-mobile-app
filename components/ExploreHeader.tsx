dd8b14ab62b9b517363b0d2527a049e032dcf398
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-24 12:19:22 +0530

    updated

--- components/ExploreHeader.tsx
+++ components/ExploreHeader.tsx
@@ -85,6 +85,7 @@ const ExploreHeader = () => {
                 {categories.map((item, index) => (
                     <TouchableOpacity 
                         key={index}
+                        ref={}
                     >
                         <MaterialIcons size={24} name={item.icon as any} /> 
                         <Text>{item.name}</Text>
