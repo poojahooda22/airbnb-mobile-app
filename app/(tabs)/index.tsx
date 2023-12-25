b45c49133f47ddbc27f720b59d13984a1ad70869
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-25 21:47:46 +0530

    updated tab

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,27 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, {useState, useMemo} from 'react'
 import { Link, Stack } from 'expo-router';
 import ExploreHeader from '@/components/ExploreHeader';
 import Listings from '@/components/Listings';
+import listingsData from '@/assets/data/airbnb-listings.json';
 
 const Page = () => {
+  const [category, setCategory] = useState('Tiny homes');
+
+  const items = useMemo(() => listingsData as any, []);
+
+  const onDataChanged = (category: string) => {
+    setCategory(category);
+  };
+
   return (
-    <View style={{ flex: 1 }}>
+    <View style={{ flex: 1, marginTop: 130 }}>
       <Stack.Screen 
         options={{
-          header: () => <ExploreHeader/>
+          header: () => <ExploreHeader onCategoryChanged={onDataChanged}/>
         }}
       />
-      <Listings />
+      <Listings listings={items} category={category}/>
     </View>
   )
 }
