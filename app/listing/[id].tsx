974f51d4943198c5f30417a3f76e010cfd8d1a58
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-26 20:41:09 +0530

    updated

--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -1,15 +1,34 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, StyleSheet, Dimensions} from 'react-native'
 import React from 'react'
-import { useLocalSearchParams } from 'expo-router'
+import { useLocalSearchParams } from 'expo-router';
+
+
+
+const IMG_HEIGHT = 300;
+
+const {width} = Dimensions.get('window');
 
 const Page = () => {
     const { id } = useLocalSearchParams<{id: string}>();
-    console.log('~ file: [id].tsx:7 ~ Page ~ id:', id);
+    const listing: Listing = (listingsData as any[]).find((item) => item.id === id);
   return (
-    <View>
-      <Text>Page</Text>
+    <View style={styles.container}>
+      <Animated.ScrollView>
+        <Animated.Image source={{ uri: listing.xl_picture_url}} style={styles.image} />
+      </Animated.ScrollView>
     </View>
   )
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  image: {
+    height: IMG_HEIGHT,
+    width,
+  }
+});
 
 export default Page;
