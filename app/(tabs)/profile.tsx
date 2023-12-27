17733aeaf3ef89c891866889e1e791a2aac4ffbb
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-27 20:41:02 +0530

    updated profile

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,13 +1,15 @@
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button, StyleSheet } from 'react-native'
 import React from 'react';
 import { useAuth } from "@clerk/clerk-react";
 import {Link} from 'expo-router';
+import { defaultStyles } from '@/constants/Styles';
+import Colors from '@/constants/Colors';
 
 const Page = () => {
   const { signOut, isSignedIn } = useAuth();
   return (
-    <View>
-      <Button title="Log out" onPress={() => signOut()}/>
+    <View style={styles.topView}>
+      <Text  onPress={() => signOut()} style={styles.button}>Logout</Text>
         {!isSignedIn && (
           <Link href={'/(modals)/login'}>
             <Text>Log In</Text>
@@ -15,6 +17,27 @@ const Page = () => {
         )}
     </View>
   )
-}
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: Colors.primary,
+    height: 50,
+    width: 119,
+    borderRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    padding: 12,
+    color: '#fff',
+    fontSize: 16,
+  },
+  topView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 16,
+  }
+});
 
 export default Page;
