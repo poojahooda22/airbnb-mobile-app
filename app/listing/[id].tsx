13a89df8bae4e13d41a73ca6932e5240f572362f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-27 21:07:52 +0530

    updated

--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -36,7 +36,7 @@ const Page = () => {
     useLayoutEffect(() => {
       navigation.setOptions({
         headerBackground: () => (
-          <Animated.View style={} />
+          <Animated.View style={styles.header} />
         ),
         headerRight: () => (
           <View style={styles.bar}>
