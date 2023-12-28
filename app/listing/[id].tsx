e32e144de5342b3625ec7eb926991217651ef59f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-28 22:31:18 +0530

    updated

--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -49,7 +49,7 @@ const Page = () => {
             </TouchableOpacity>
             <TouchableOpacity style={styles.roundButton} >
               <Ionicons 
-              name="heart-outline" size={22} color={'#000'} />
+                name="heart-outline" size={22} color={'#000'} />
             </TouchableOpacity>
           </View>
         ),
