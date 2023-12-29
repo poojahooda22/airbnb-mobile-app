a1a4e14ec9c66814a1c0c8dd4c7e7990146f26fa
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-29 11:54:07 +0530

    updated

--- components/ListingsMap.tsx
+++ components/ListingsMap.tsx
@@ -1,16 +1,25 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { defaultStyles } from '@/constants/Styles';
 
 
 interface Props {
     listings: any;
 }
 
+const INITIAL_REGION = {
+  latitude: 37.33,
+  longitude: -122,
+  latitudeDelta: 9,
+  longitudeDelta: 9,
+}
+
 const ListingsMap = ({listings}: Props) => {
   return (
     <View style={styles.container}>
-        <MapView style={styles.map } provider={PROVIDER_GOOGLE} showsUserLocation showsMyLocationButton />
+        <MapView 
+        style={styles.map } provider={PROVIDER_GOOGLE} showsUserLocation showsMyLocationButton />
     </View>
   );
 };
