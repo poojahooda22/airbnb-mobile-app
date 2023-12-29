839dfd3b0603f7bb669add5c8542664e4ce3d7aa
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-30 00:18:47 +0530

    inbox

--- app/(tabs)/inbox.tsx
+++ app/(tabs)/inbox.tsx
@@ -1,10 +1,15 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { Ionicons } from '@expo/vector-icons';
 
 const Page = () => {
   return (
-    <View>
-      <Text>Inbox</Text>
+    <View style={{ flexDirection: 'column', alignItems: 'center', gap: 4, margin: 16, paddingVertical: 200}}>
+      <Ionicons name="chatbox-outline" size={24} color="black" />
+      <Text style={{ textAlign: 'center', fontFamily: 'mon-sb', fontSize: 16, paddingBottom: 4}}>No new messages</Text>
+      <Text style={{ textAlign: 'center', fontFamily: 'mon', fontSize: 14}}>
+        When you contact a Host or send a reservation request, you'll see your messages here.
+      </Text>
     </View>
   )
 }
