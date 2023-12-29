43bcc4f7500dc031a69a9875dedea122e83be072
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-29 11:34:16 +0530

    components

--- components/ListingsMap.tsx
+++ components/ListingsMap.tsx
@@ -1,14 +1,28 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
-import MapView from 'react-native-maps'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
 
 
-const ListingsMap = () => {
-  return (
-    <View>
-        <Text>Listing Map</Text>
-    </View>
-  )
+interface Props {
+    listings: any;
 }
+
+const ListingsMap = ({listings}: Props) => {
+  return (
+    <View style={styles.container}>
+        <MapView style={styles.map } provider={PROVIDER_GOOGLE} showsUserLocation showsMyLocationButton />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
 
 export default ListingsMap
