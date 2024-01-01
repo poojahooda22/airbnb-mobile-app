225135ce7a2603d5871cabdb35c8871f6ec7fc6c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-01 23:14:40 +0530

    updated

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -119,7 +119,7 @@ const Page = () => {
           >
             <Ionicons name="person-circle" size={24} style={defaultStyles.btnIcon}/>
             <Text>Personal information</Text>
-              <Ionicons name="chevron-forward-outline" size={24} style={defaultStyles.btnIcon}/>
+            <Ionicons name="chevron-forward-outline" size={24} style={defaultStyles.btnIcon}/>
           </View>
         </View>
       
