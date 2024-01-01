b81ef78cc7b0f8af10c6668804d783ee277e1dd1
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-01 23:13:37 +0530

    updated

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -114,7 +114,8 @@ const Page = () => {
           >
             Settings
           </Text>
-          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+          <View 
+          style={{flexDirection: 'row', justifyContent: 'space-between'}}>
               <Ionicons name="person-circle" size={24} style={defaultStyles.btnIcon}/>
               <Text>Personal information</Text>
               <Ionicons name="chevron-forward-outline" size={24} style={defaultStyles.btnIcon}/>
