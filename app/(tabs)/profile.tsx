0c286a9adb15316d26b85e219a4b491bf00e3704
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-01 23:13:06 +0530

    updated

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -112,7 +112,8 @@ const Page = () => {
           <Text 
             style={{fontFamily: 'mon-sb', fontSize: 18, marginBottom: 30}}
           >
-            Settings</Text>
+            Settings
+            </Text>
           <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
               <Ionicons name="person-circle" size={24} style={defaultStyles.btnIcon}/>
               <Text>Personal information</Text>
