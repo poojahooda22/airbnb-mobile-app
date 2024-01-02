6a67248be0c9488b37226835ad2432be78ff0b0a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-02 22:00:35 +0530

    updated

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -50,7 +50,8 @@ const Page = () => {
   }
 
   return (
-    <SafeAreaView style={{ marginTop: 30}}>
+    <SafeAreaView 
+    style={{ marginTop: 30}}>
       <View style={styles.headerContainer}>
         <Text style={styles.header}>Profile</Text>
         <Ionicons name="notifications-outline" size={24} />
